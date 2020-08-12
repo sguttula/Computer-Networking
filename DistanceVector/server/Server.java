ff9258394352a596993ee44905383793b381a5b6
Sudarshan Guttula <sidguttula@yahoo.com>
2020-08-12 12:25:43 -0700

    Inet Socket Address server

---
+++ DistanceVector/server/Server.java
@@ -0,0 +1,43 @@
+package server;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+import model.dv;
+
+public class Server extends Thread{
+	private int port = 0;
+	public Server(int port)
+    {
+       this.port = port;
+    }
+	public void run() {
+        try
+        {
+            dv.read = Selector.open();
+            dv.write = Selector.open();
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.bind(new InetSocketAddress(port));
+            while(true)
+			{
+				SocketChannel socketChannel=serverSocketChannel.accept();
+				if(socketChannel != null)
+				{
+					socketChannel.configureBlocking(false);
+					socketChannel.register(dv.read, SelectionKey.OP_READ);
+					socketChannel.register(dv.write, SelectionKey.OP_WRITE);
+					dv.openChannels.add(socketChannel);
+					System.out.println("The connection to peer "+dv.parseChannelIp(socketChannel)+" is succesfully established");
+				}
+			}
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+	}
+}
